2a500695153f36e97287acaa0127d3ec73abb0cc
Marcus Kruse <esurk.sucram@gmail.com>
2020-05-02 15:28:49 +0200

    Add travis

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BencheeDsl.MixProject do
     [
       app: :benchee_dsl,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
