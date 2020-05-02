3104c252255b1985a51f910ea7b79fec38315a04
Marcus Kruse <esurk.sucram@gmail.com>
2020-05-02 20:39:05 +0200

    Make the parent project available

--- lib/mix/tasks/bench.ex
+++ lib/mix/tasks/bench.ex
@@ -5,6 +5,7 @@ defmodule Mix.Tasks.Bench do
 
   @shortdoc "Start and runs the benchmarks"
   def run(_) do
+    Mix.Task.run "compile"
     Application.ensure_all_started(:benchee_dsl)
     Code.compile_file("benchee_helper.exs", "bench")
   end
