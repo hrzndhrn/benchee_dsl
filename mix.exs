d8fa10426cac27709d35a2e531defe9ba8f8724c
Marcus Kruse <esurk.sucram@gmail.com>
2020-05-04 20:01:05 +0200

    Update supported version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule BencheeDsl.MixProject do
     [
       app: :benchee_dsl,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       consolidate_protocols: true,
